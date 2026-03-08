f70378ceb7a4d120ef5e3f1cfa69c9a1cb786b71
SpaceXEngineer <148818352+FalconDevX@users.noreply.github.com>
2026-03-08 18:47:17 +0100

    refactor main and router files to streamline FastAPI setup and remove unnecessary imports

--- app/api/router.py
+++ app/api/router.py
@@ -1,12 +1,8 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter
 from app.services.gios_service import get_stations
 
-app = FastAPI()
 router = APIRouter()
-
 
 @router.get("/gios/stations", tags=["gios"])
 async def get_gios_stations():
     return get_stations()
-
-app.include_router(router)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from app.api.router import router
-from app.services.gios_service import get_stations
-
 
 app = FastAPI()
 
 app.include_router(router)
-
