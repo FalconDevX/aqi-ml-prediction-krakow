91656c51e9d85595f673e59fd75bd59c84bafc3f
WhatAPity171 <153460762+WhatAPity171@users.noreply.github.com>
2026-03-16 19:57:50 +0100

    Data agregation from dataset added

--- app/services/get_data_from_dataset.py
+++ app/services/get_data_from_dataset.py
@@ -21,13 +21,28 @@ def download_data_from_dataset():
 
 def extract_data_from_dataset(year:int, param:str):
     #im sure theres better way to mark the path but i dontthink its important now
+    station = "MpKrakBujaka"
+    if param == "CO":
+        station = "MpKrakBulwar" #exception for CO there is no CO sensor on Bujaka
     data = pd.read_csv(DATA_FROM_KAGGLE_FOLDER/f"datasets/wisekinder/poland-air-quality-monitoring-dataset-2017-2023/versions/14/{year}/{year}_{param}_1g.csv")
     if param == "PM25": #only for this one it is fuking different
         param = "PM2.5"
-    data = data[["Time",f"MpKrakBujaka-{param}-1g"]]
-    print(data.head())
+        station = "MpKrakBulwar"
+    data = data[["Time",f"{station}-{param}-1g"]]
+    #print(data.head())
     return data
+
+def extract_all_data_from_dataset():
+    years = [2017, 2018, 2019, 2020, 2021, 2022, 2023]
+    data = pd.DataFrame()
+    params = ["SO2", "NO2", "CO", "O3", "PM10", "PM25"]
+    for year in years:
+        for param in params:
+            os.makedirs(f"data/{year}", exist_ok=True)
+            data = extract_data_from_dataset(year, param)
+            data.to_csv(f"data/{year}/{year}_{param}.csv", index=False)
+            #return data
 
 #path = download_data_from_dataset()
 #print(path)
-extract_data_from_dataset(2023, "SO2")
+extract_all_data_from_dataset()
