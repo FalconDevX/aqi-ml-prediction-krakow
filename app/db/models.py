10919082eb2791436abdc7b7ca8e2869ffbab6b4
WhatAPity171 <153460762+WhatAPity171@users.noreply.github.com>
2026-03-24 18:05:58 +0100

    Added to postgre api, can't really test because DB is acting up, and i cant access it with new env

--- app/db/models.py
+++ app/db/models.py
@@ -7,7 +7,7 @@ from datetime import datetime
 # sqlalchemy models for our postgres database
 class stations(Base):
     __tablename__ = "stations"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     latitude = Column(Float)
     longitude = Column(Float)
