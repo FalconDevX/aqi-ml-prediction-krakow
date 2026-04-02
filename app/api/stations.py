9abd795c41c638e12385fba4b1723335154a7926
SpaceXEngineer <148818352+FalconDevX@users.noreply.github.com>
2026-04-02 21:31:43 +0200

    Add data collection endpoint and enhance data saving functionality
    - Introduced a new endpoint `/collect-data` in `stations.py` to trigger data collection for all stations.
    - Enhanced `save_all_data_stations_24h` in `airly_service.py` to save current and historical data in structured directories with timestamps.
    - Improved error handling during data retrieval to skip stations that encounter issues while saving data.

--- app/api/stations.py
+++ app/api/stations.py
@@ -1,7 +1,17 @@
 from fastapi import APIRouter
-from app.services.airly_service import get_current_data_from_station
+from app.services.airly_service import (
+    get_current_data_from_station,
+    save_all_data_stations_24h,
+)
 
 router = APIRouter()
+
+
+@router.get("/collect-data")
+async def collect_data():
+    await save_all_data_stations_24h()
+    return {"status": "ok"}
+
 
 @router.get("/current/{station_id}")
 async def get_curr_station_data(station_id: int):
